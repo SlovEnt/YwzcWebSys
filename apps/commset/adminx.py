ded16e014082828b08f11dd5cfc1d7fdc42d5cc1
SlovEnt <slovent@163.com>
2018-03-26 00:50:44 +0800

    完成IP地址管理功能APP up01

--- apps/commset/adminx.py
+++ apps/commset/adminx.py
@@ -6,18 +6,18 @@ from xadmin.models import Log
 
 # 和X admin的view绑定
 from xadmin import views
-from .models import SysDictName, SysDictItem
+from .models import UserProfile, SysDictName, SysDictItem
 
 
 # x admin 全局配置参数信息设置
-class GlobalSettings(object):
-    site_title = "业务支持综合管理系统"
-    site_footer = "SlovEnt 提供技术支持"
-    # 收起菜单
-    menu_style = "accordion"
-
-# 将头部与脚部信息进行注册:
-xadmin.site.register(views.CommAdminView, GlobalSettings)
+# class GlobalSettings(object):
+#     site_title = "业务支持综合管理系统"
+#     site_footer = "SlovEnt 提供技术支持"
+#     # 收起菜单
+#     menu_style = "accordion"
+#
+# # 将头部与脚部信息进行注册:
+# xadmin.site.register(views.CommAdminView, GlobalSettings)
 
 
 # 系统字典
@@ -34,6 +34,7 @@ class SysDictNameAdmin(object):
     inlines = [SysDictInline]
     refresh_times = [3, 5]
 
+
 xadmin.site.register(SysDictName, SysDictNameAdmin)
 
 class SysDictItemAdmin(object):
@@ -44,3 +45,23 @@ class SysDictItemAdmin(object):
 
 xadmin.site.register(SysDictItem, SysDictItemAdmin)
 
+
+class GlobalSetting(object):
+
+    site_title = "业务支持综合管理系统"
+    site_footer = "SlovEnt 提供技术支持"
+    menu_style = "accordion"
+
+    def get_site_menu(self):
+        return (
+            {'title': '系统管理', 'menus': (
+                {'title': '用户信息', 'url': self.get_model_url(UserProfile, 'changelist')},
+                {'title': '用户分组', 'url': self.get_model_url(Group, 'changelist')},
+                {'title': '用户权限', 'url': self.get_model_url(Permission, 'changelist')},
+                {'title': '系统字典', 'url': self.get_model_url(SysDictName, 'changelist')},
+                {'title': '字典类别', 'url': self.get_model_url(SysDictItem, 'changelist')},
+                {'title': '日志记录', 'url': self.get_model_url(Log, 'changelist')},
+            )},
+        )
+
+xadmin.site.register(views.CommAdminView, GlobalSetting)
