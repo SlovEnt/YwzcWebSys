5773d6980cf477fdee31dfba0217ce7a61dc2e4e
SlovEnt <slovent@163.com>
2018-03-28 00:36:13 +0800

    NetCommit

--- apps/insbusi/models.py
+++ apps/insbusi/models.py
@@ -17,14 +17,14 @@ class InserNewOrgInfo(models.Model):
     tel = models.CharField(max_length=16, default="0", verbose_name="联系电话")
     sfc_code = models.CharField(max_length=8, verbose_name="证监会代码")
     sse_code = models.CharField(max_length=8, verbose_name="上交所唯一码")
-    sza_ic = models.CharField(max_length=4, verbose_name="深A标识码")
     sza_unit = models.CharField(max_length=8, verbose_name="深A交易单元")
+    sza_ic = models.CharField(max_length=4, verbose_name="深A标识码")
     sza_trd_interval = models.CharField(max_length=32, verbose_name="深A交易区间")
     szb_trd_interval = models.CharField(max_length=32, verbose_name="深B交易区间")
     sta_trd_interval = models.CharField(max_length=32, verbose_name="股转A交易区间")
     szhk_trd_interval = models.CharField(max_length=32, verbose_name="深港通交易区间")
-    sha_ic = models.CharField(max_length=4, verbose_name="沪A标识码")
     sha_unit = models.CharField(max_length=8, verbose_name="沪A交易单元")
+    sha_ic = models.CharField(max_length=4, verbose_name="沪A标识码")
     sha_trd_interval = models.CharField(max_length=32, verbose_name="沪A交易区间")
     shb_trd_interval = models.CharField(max_length=32, verbose_name="沪B交易区间")
     central_org = models.CharField(max_length=8, blank=True, verbose_name="中心营业部编码")
@@ -33,6 +33,11 @@ class InserNewOrgInfo(models.Model):
         verbose_name = u"批量加新营业部"
         verbose_name_plural = verbose_name
         ordering = ('org_code',)
+
+    # 重载__str__方法，打印实例会打印username，username为继承自AbstractUser
+    def __str__(self):
+        return "营业部编码为：%s 的记录" % self.org_code
+
 
 
 class InserNewOrgInfoLog(models.Model):
@@ -44,3 +49,26 @@ class InserNewOrgInfoLog(models.Model):
     class Meta:
         verbose_name = u"营业部添加日志"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "%s已处理 %s " % (self.oper_sys, self.org_code)
+
+
+class RunScriptsList(models.Model):
+    script_name = models.CharField(max_length=128, verbose_name="函数名称")
+    run_status = models.CharField(max_length=1, choices=GetSysDict("RUN_STATUS"), default="0", verbose_name="执行状态")
+    run_path = models.TextField(verbose_name="执行路径")
+
+    def run_point(self):
+
+        from django.utils.safestring import mark_safe
+        return mark_safe('''<a style="padding:2px 12px;" href="/xadmin/insbusi/%s/run/" class="btn btn-primary">执行</a>''')
+        # return mark_safe('''<a href="/xadmin/insbusi/%s/run/" class="btn btn-primary">执行</a>''')
+    run_point.short_description = "执行"
+
+    class Meta:
+        verbose_name = u"函数执行表"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "%s" % (self.script_name)
